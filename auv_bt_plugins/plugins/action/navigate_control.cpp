7b3c8102399606abd6fe50dcd1640f7d69c76400
Amadeus <amadeusw6@gmail.com>
2021-07-08 22:42:15 +0800

    Added plugins for buoy task

---
+++ auv_bt_plugins/plugins/action/navigate_control.cpp
@@ -0,0 +1,78 @@
+#include <memory>
+#include <string>
+
+#include "auv_bt_plugins/action/navigate_control.hpp"
+#include "tf2_geometry_msgs/tf2_geometry_msgs.h"
+#include <tf2/LinearMath/Quaternion.h>
+#include <tf2/LinearMath/Matrix3x3.h>
+
+
+namespace mp_behavior_tree
+{
+
+NavigateControl::NavigateControl(
+    const std::string & xml_tag_name,
+    const std::string & action_name,
+    const BT::NodeConfiguration & conf)
+  : BtActionNode<nav_utils::LocomotionAction, 
+                 nav_utils::LocomotionGoal, 
+                 nav_utils::LocomotionResult>(xml_tag_name, action_name, conf) {}
+
+void NavigateControl::on_tick() {
+    tf2::Quaternion quat;
+    float forward, sideways, depth, yaw;
+
+    double x, y, z;
+    bool movement_rel = true;
+
+    if (!getInput("forward", forward)) {
+        ROS_WARN("[NavigateControl] forward not provided! Station-keeping!");
+        forward = 0;
+    }
+
+    if (!getInput("sideways", sideways)) {
+        ROS_WARN("[NavigateControl] sideways not provided! Station-keeping!");
+        sideways = 0;
+    }
+
+    if (!getInput("depth", depth)) {
+        ROS_WARN("[NavigateControl] depth not provided! Station-keeping!");
+        depth = 0;
+    }
+
+    if (!getInput("yaw", yaw)) {
+        ROS_WARN("[NavigateControl] yaw not provided! Station-keeping!");
+        yaw = 0;
+    }
+
+    // Using NED Convention
+    goal_.forward_setpoint = forward;
+    goal_.sidemove_setpoint = sideways;
+    goal_.depth_setpoint = depth;
+    goal_.yaw_setpoint = yaw;
+
+    // Setting up relative motion
+    goal_.yaw_rel = true;
+    goal_.movement_rel = true;
+    goal_.controller_state = 0;
+}
+
+void NavigateControl::on_wait_for_result() {
+
+}
+
+} // namespace mp_behavior_tree
+
+#include "behaviortree_cpp_v3/bt_factory.h"
+BT_REGISTER_NODES(factory)
+{
+  BT::NodeBuilder builder =
+    [](const std::string & name, const BT::NodeConfiguration & config)
+    {
+      return std::make_unique<mp_behavior_tree::NavigateControl>(
+        name, "Locomotion", config);
+    };
+
+  factory.registerBuilder<mp_behavior_tree::NavigateControl>(
+    "NavigateControl", builder);
+}
---
+++ buoy_bt_plugins/include/buoy_bt_plugins/action/find_buoy.hpp
@@ -0,0 +1,34 @@
+#ifndef BUOY_BT_PLUGINS__PLUGINS__ACTION__FIND_BUOY_HPP_
+#define BUOY_BT_PLUGINS__PLUGINS__ACTION__FIND_BUOY_HPP_
+
+#include <ros/ros.h>
+#include "vision/DetectedObjects.h"
+#include <behaviortree_cpp_v3/action_node.h>
+
+namespace mp_behavior_tree
+{
+class FindBuoy : public BT::SyncActionNode {
+public : 
+    FindBuoy(
+        const std::string & xml_tag_name,
+        const BT::NodeConfiguration &conf);
+
+    ~FindBuoy() {}
+
+    static BT::PortsList providedPorts() 
+    {
+        return {
+            BT::InputPort<vision::DetectedObjects>("vision_objects", "Objects detected by vision pipeline as a vision::DetectedObjects message"),
+            BT::InputPort<std::string>("target_identity", "Target to be made as goal. String"),
+            BT::OutputPort<float>("forward", "Forward as float"),
+            BT::OutputPort<float>("sideways", "Sideways as float"),
+            BT::OutputPort<float>("depth", "Depth as float"),
+            BT::OutputPort<float>("yaw", "Yaw as float"),
+        };
+    }
+
+    BT::NodeStatus tick() override;
+};
+} // namespace mp_behavior_tree
+
+#endif
