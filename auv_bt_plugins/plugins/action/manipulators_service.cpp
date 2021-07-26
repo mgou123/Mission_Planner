0b39e9228ed6faff6e8135389371d5854ac0765f
Amadeus <amadeusw6@gmail.com>
2021-07-26 21:30:52 +0800

    Fix manipulators

--- auv_bt_plugins/plugins/action/manipulators_service.cpp
+++ auv_bt_plugins/plugins/action/manipulators_service.cpp
@@ -13,12 +13,33 @@ ManipulatorsService::ManipulatorsService(
   : BtServiceNode<bbauv_msgs::Manipulators>(xml_tag_name, service_name, conf) {}
 
 void ManipulatorsService::on_tick() {
-    getInput("top_torpedo", srv_->request.top_torpedo);
-    getInput("bottom_torpedo", srv_->request.bottom_torpedo);
-    getInput("dropper", srv_->request.dropper);
-    getInput("grabber", srv_->request.grabber);
-    getInput("extend_linear", srv_->request.extend_linear);
-    getInput("retract_linear", srv_->request.retract_linear);
+    bool flag;
+    if (getInput("top_torpedo", flag)) {
+      srv_->request.top_torpedo = flag;
+    }
+    if (getInput("bottom_torpedo", flag)) {
+      srv_->request.bottom_torpedo = flag;
+    }
+    if (getInput("dropper", flag)) {
+      srv_->request.dropper = flag;
+    }
+    if (getInput("grabber", flag)) {
+        ROS_WARN("[ManipulatorsService]: If you're trying to open grabber use extend_linear instead.");
+      srv_->request.grabber = flag;
+    }
+    if (getInput("extend_linear", flag)) {
+      srv_->request.extend_linear = flag;
+    }
+    if (getInput("retract_linear", flag)) {
+      srv_->request.retract_linear = flag;
+    }
+
+    // getInput("top_torpedo", srv_->request.top_torpedo);
+    // getInput("bottom_torpedo", srv_->request.bottom_torpedo);
+    // getInput("dropper", srv_->request.dropper);
+    // getInput("grabber", srv_->request.grabber);
+    // getInput("extend_linear", srv_->request.extend_linear);
+    // getInput("retract_linear", srv_->request.retract_linear);
     
 }
 
