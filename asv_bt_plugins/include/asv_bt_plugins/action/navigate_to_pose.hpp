39474390b9f11f36f4205b943bd0890077639dc2
Amadeus <amadeusw6@gmail.com>
2021-12-10 01:16:48 +0800

    Added basic asv plugin

---
+++ asv_bt_plugins/include/asv_bt_plugins/action/navigate_to_pose.hpp
@@ -0,0 +1,45 @@
+#ifndef ASV_BT_PLUGINS__PLUGINS__ACTION__NAVIGATE_TO_POSE_HPP_
+#define ASV_BT_PLUGINS__PLUGINS__ACTION__NAVIGATE_TO_POSE_HPP_
+
+#include <string>
+#include "geometry_msgs/PoseStamped.h"
+#include "bb_msgs/LocomotionAction.h"
+#include "bb_msgs/LocomotionGoal.h"
+#include "bb_msgs/LocomotionResult.h"
+#include "mp_behavior_tree/bt_action_node.hpp"
+
+// mp wrapper for Locomotion action
+namespace mp_behavior_tree
+{
+
+class NavigateToPose 
+: public BtActionNode<bb_msgs::LocomotionAction, 
+                      bb_msgs::LocomotionGoal, 
+                      bb_msgs::LocomotionResult>
+{
+public:
+    NavigateToPose(
+        const std::string & xml_tag_name,
+        const std::string & action_name,
+        const BT::NodeConfiguration & conf);
+    
+    void on_tick() override;
+
+    static BT::PortsList providedPorts()
+    {
+        return providedBasicPorts(
+        {
+            BT::InputPort<geometry_msgs::PoseStamped>("goal", "Destination to plan to"),
+            BT::InputPort<double>("yaw_lock", "Yaw to lock to. If specified, relative yaw in pose will be ignored."),
+            BT::InputPort<bool>("relative", "Set to true to use relative coordinates"),
+            BT::InputPort<double>("forward_tolerance", "Tolerance for forward motion"),
+            BT::InputPort<double>("sidemove_tolerance", "Tolerance for XY motion"),
+            BT::InputPort<double>("yaw_tolerance", "Tolerance for yaw")
+
+        });
+    }
+};
+
+} // namespace mp_behavior_tree
+
+#endif
---
+++ asv_bt_plugins/plugins/action/navigate_to_pose.cpp
@@ -0,0 +1,92 @@
+#include <string>
+
+#include "asv_bt_plugins/action/navigate_to_pose.hpp"
+#include "tf2_geometry_msgs/tf2_geometry_msgs.h"
+#include <tf2/LinearMath/Quaternion.h>
+#include <tf2/LinearMath/Matrix3x3.h>
+
+namespace mp_behavior_tree
+{
+
+NavigateToPose::NavigateToPose(
+    const std::string & xml_tag_name,
+    const std::string & action_name,
+    const BT::NodeConfiguration & conf)
+  : BtActionNode<bb_msgs::LocomotionAction, 
+                 bb_msgs::LocomotionGoal, 
+                 bb_msgs::LocomotionResult>(xml_tag_name, action_name, conf) {}
+
+void NavigateToPose::on_tick() {
+    tf2::Quaternion quat;
+    geometry_msgs::PoseStamped goal_pose_stamped;
+    double roll, pitch, yaw;
+    bool rel;
+
+    if (!getInput("goal", goal_pose_stamped)) {
+        ROS_ERROR("[NavigateToPose] goal not provided!");
+        return;
+    }
+
+    // set goal
+    tf2::convert(goal_pose_stamped.pose.orientation, quat);
+    tf2::Matrix3x3(quat).getRPY(roll, pitch, yaw);
+
+    goal_.forward_setpoint = goal_pose_stamped.pose.position.x;
+    goal_.sidemove_setpoint = goal_pose_stamped.pose.position.y;
+
+    goal_.yaw_setpoint = yaw / M_PI * 180.0;
+
+    //set relative
+    bool relative;
+    if (getInput("relative", relative)) {
+      if (relative) {
+        goal_.movement_rel = true;
+        goal_.yaw_rel = true;
+      } else {
+        goal_.movement_rel = false;
+        goal_.yaw_rel = false;
+      }
+    } else {
+      goal_.movement_rel = true;
+      goal_.yaw_rel = true;
+    } 
+
+    if (!getInput("yaw_lock_relative", rel)) {
+      ROS_WARN("[NavigateToPose]: yaw_lock_relative not passed. Defaults to false");
+      rel = false;
+    }
+
+    if (getInput("yaw_lock", yaw)) {
+      goal_.yaw_rel = rel;
+      goal_.yaw_setpoint = yaw;
+    }
+
+    double sidemove_tolerance, forward_tolerance, yaw_tolerance;
+
+    if (getInput("sidemove_tolerance", sidemove_tolerance)) {
+      goal_.sidemove_tolerance = sidemove_tolerance;
+    }
+
+    if (getInput("forward_tolerance", forward_tolerance)) {
+      goal_.forward_tolerance = forward_tolerance;
+    }
+
+    if (getInput("yaw_tolerance", yaw_tolerance)) {
+      goal_.yaw_tolerance = yaw_tolerance;
+    }
+}
+
+} // namespace mp_behavior_tree
+
+#include "behaviortree_cpp_v3/bt_factory.h"
+BT_REGISTER_NODES(factory) {
+  BT::NodeBuilder builder =
+    [](const std::string & name, const BT::NodeConfiguration & config)
+    {
+      return std::make_unique<mp_behavior_tree::NavigateToPose>(
+        name, "Locomotion", config);
+    };
+
+  factory.registerBuilder<mp_behavior_tree::NavigateToPose>(
+    "NavigateToPose", builder);
+}
